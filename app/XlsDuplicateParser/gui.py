a928fa76d63adfd90a320dff70c9f295df2c93b2
gabmichelet <gabmichelet@bluewin.ch>
2017-01-17 10:31:45 +0100

    update

--- app/XlsDuplicateParser/gui.py
+++ app/XlsDuplicateParser/gui.py
@@ -1,56 +1,36 @@
 # coding: utf-8
 
 import tkinter as tk
-from tkinter.messagebox import *
-from tkinter.filedialog import *
-from app.XlsDuplicateParser.controller import XlsDuplicateParser
 import subprocess as sub
 import pyexcel as p
+from tkinter import font
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+
+from app.XlsDuplicateParser.controller import XlsDuplicateParser
 
 
 class XlsDuplicateParserGUI():
     def __init__(self, master):
         self.master = master
         self.frame = tk.Frame(self.master)
+        self.frame.configure(padx=20, pady=10)
         self.controller = XlsDuplicateParser()
+        self.customFont = font.Font(family="Helvetica", size=20)
 
         self.init_menu()
         self.init_action_buttons()
+        self.init_labels()
+        self.init_files_field()
         self.init_output()
 
-        self.row_where_add_file = 5
-
         self.frame.grid()
-
-    def add_file(self):
-        file_name = askopenfilename(
-            title='Choose a file to compare',
-            filetypes=[
-                ('xls files', '.xls'),
-                ('xlsx files', '.xlsx'),
-                ('all files', '.*')
-            ]
-        )
-        if file_name:
-            self.controller.add_sheet_to_compare(file_name)
-    
-            files_label = tk.Label(
-                self.frame,
-                text="File to analyze : {}".format(file_name)
-            )
-            files_label.grid(row=self.row_where_add_file, column=0, sticky=W+E)
-    
-            self.row_where_add_file += 1
-
-    def clear_files(self):
-        del self.controller.sheets_to_compare[:]
-        self.out.delete(0, 'end')
 
     def init_menu(self):
         self.menubar = Menu(self.master)
         self.menu1 = Menu(self.menubar, tearoff=0)
         self.menu1.add_command(
-            label="Import file to compare",
+            label="Add file to compare",
             command=lambda: self.add_file()
         )
         self.menu1.add_separator()
@@ -63,62 +43,100 @@ class XlsDuplicateParserGUI():
             self.frame,
             text='ADD FILE',
             width=30,
+            bg='blue',
+            font=self.customFont,
             command=lambda: self.add_file()
         )
-        self.add_file_button.grid(row=0, column=0, columnspan=6)
+        self.add_file_button.grid(row=0, column=0, columnspan=2)
 
         self.analyze_button = tk.Button(
             self.frame,
             text='FIND OCCURENCES',
             width=30,
+            font=self.customFont,
             command=lambda: self.analyze()
         )
-        self.analyze_button.grid(row=1, column=0, columnspan=6)
+        self.analyze_button.grid(row=1, column=0, columnspan=2)
 
-        self.add_file_button = tk.Button(
+        self.clear_files_button = tk.Button(
             self.frame,
             text='CLEAR FILES',
             width=30,
+            font=self.customFont,
             command=lambda: self.clear_files()
         )
-        self.add_file_button.grid(row=2, column=0, columnspan=6)
+        self.clear_files_button.grid(row=0, column=3, columnspan=2)
 
         self.close_button = tk.Button(
             self.frame,
             text='QUIT',
             width=30,
+            font=self.customFont,
             command=lambda: self.close_windows()
         )
-        self.close_button.grid(row=3, column=0, columnspan=6)
+        self.close_button.grid(row=1, column=3, columnspan=2)
+
+    def init_labels(self):
+        self.label1 = tk.Label(
+            self.frame,
+            text='Fichiers sélectionnés',
+            font=self.customFont
+        )
+        self.label1.grid(row=2, column=0, sticky=W)
+
+        self.label2 = tk.Label(
+            self.frame,
+            text='Occurences trouvées',
+            font=self.customFont
+        )
+        self.label2.grid(row=2, column=3, sticky=W)
+
+    def init_files_field(self):
+        self.files_field = tk.Text(
+            self.frame,
+            bg='grey',
+            padx=10,
+            pady=10,
+            font=self.customFont
+        )
+        self.files_field.grid(row=3, column=0, columnspan=2)
 
     def init_output(self):
-        self.out = tk.Text(self.frame)
-        self.out.grid(row=0, rowspan=13, column=8, columnspan=6)
+        self.out = tk.Text(
+            self.frame,
+            bg='grey',
+            padx=10,
+            pady=10,
+            font=self.customFont
+        )
+        self.out.grid(row=3, column=3, columnspan=2)
 
-    def close_windows(self):
-        """ Close an app window """
-        self.master.destroy()
-
-    def analyze(self):
-        result = self.controller.browse_rows_multiple_files(self.controller.sheets_to_compare)
-        for r in result:
-            self.out.insert('end', 'MATCH : {}\n'.format(r))
-
-    def openfiledialog(self, title, target):
-        filename = askopenfilename(
-            title=title,
+    def add_file(self):
+        file_name = askopenfilename(
+            title='Choose a file to compare',
             filetypes=[
                 ('xls files', '.xls'),
                 ('xlsx files', '.xlsx'),
                 ('all files', '.*')
             ]
         )
-        if target == 'first':
-            self.file_name1['text'] = filename
-            self.controller.sheet1 = self.controller.set_sheet(filename)
-        elif target == 'second':
-            self.file_name2['text'] = filename
-            self.controller.sheet2 = self.controller.set_sheet(filename)
+        if file_name:
+            self.controller.add_sheet_to_compare(file_name)
+            self.files_field.insert('end', '{}\n'.format(file_name))
+
+    def clear_files(self):
+        del self.controller.sheets_to_compare[:]
+        self.out.delete(1.0, 'end')
+        self.files_field.delete(1.0, 'end')
 
     def close_windows(self):
+        """ Close an app window """
         self.master.destroy()
+
+    def analyze(self):
+        self.out.delete(1.0, 'end')
+        result = self.controller.browse_rows_multiple_files(
+            self.controller.sheets_to_compare
+        )
+        for r in result:
+            self.out.insert('end', 'MATCH : {}\n'.format(r))
