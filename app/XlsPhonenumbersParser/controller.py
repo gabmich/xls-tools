a0e1e13a1cefb9f3e1a4fe8f27eab86f02abdc52
gabmichelet <gabmichelet@bluewin.ch>
2017-01-18 22:15:39 +0100

    phonenumbers parser : multiple files support added

--- app/XlsPhonenumbersParser/controller.py
+++ app/XlsPhonenumbersParser/controller.py
@@ -6,20 +6,33 @@ import phonenumbers as pn
 
 class XlsPhonenumbersParser():
 
-    def load_file(self, file_name):
-        self.file_name = file_name
-        self.sheet = p.get_sheet(file_name=file_name)
+    def __init__(self):
+        self.sheets = []
 
-    def parse_file(self):
-        for i, row in enumerate(self.sheet):
+    def add_file(self, file_name):
+        self.sheets.append(file_name)
+
+    def parse(self):
+        for sheet in self.sheets:
+            self.parse_file(sheet)
+
+    def parse_file(self, sheet_name):
+        sheet = p.get_sheet(file_name=sheet_name)
+
+        for i, row in enumerate(sheet):
             for c, cell in enumerate(row):
+                cell = str(cell)
                 if pn.is_possible_number_string(cell, "CH"):
                     raw_number = pn.parse(cell, 'CH')
                     formated_number = pn.format_number(raw_number, pn.PhoneNumberFormat.E164)
-                    self.sheet.cell_value(i, c, new_value=formated_number)
-        self.sheet.save_as('new_file.xlsx')
+                    sheet.cell_value(i, c, new_value=formated_number)
+        a = sheet_name.split('.')
+        a.pop()
+        new_sheet_name = ''.join(a)
+
+        sheet.save_as('{}_parsed.xlsx'.format(new_sheet_name))
 
 x = XlsPhonenumbersParser()
-file_name = 'a1.xlsx'
-x.load_file(file_name)
-x.parse_file()
+x.add_file('a1.xlsx')
+x.add_file('fichier1.xlsx')
+x.parse()
